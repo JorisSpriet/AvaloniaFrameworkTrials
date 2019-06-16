8f889ee40079d88342c80a7cb1f4bde97b358dab
Joris Spriet <Joris.Spriet@brighteye.be>
2019-06-16 20:54:54 +0200

    playing around

---
+++ AvaloniaApplication1/AvaloniaApplication1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Reactive;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ReactiveUI;
+
+namespace AvaloniaApplication1
+{
+	public class MainWindow : Window
+	{
+		public MainWindow()
+		{
+
+			ButtonCommand = ReactiveCommand.Create(CommandImplementation);
+
+			InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+			
+		}
+
+		private IReactiveCommand ButtonCommand { get; set; }
+
+		private void CommandImplementation()
+		{}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
